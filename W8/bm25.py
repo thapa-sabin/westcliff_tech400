b0d746250724bc87424a40a8f0d18c2389c3dde3
Sabin Thapa <thapasabin@zohomail.com>
2024-10-21 06:56:47 +0545

    Made some aesthetic changes

--- W8/bm25.py
+++ W8/bm25.py
@@ -10,7 +10,7 @@ def compute_bm25(query, term_freq, idf, doc_count, avgdl, doc_lengths, k1=1.5, b
                 freq = tf.get(term, 0)
                 numerator = freq * (k1 + 1)
                 denominator = freq + k1 * (1 - b + b * doc_lengths[doc_id] / avgdl)
-                # scores[doc_id] += idf[term] * numerator / denominator
+                scores[doc_id] += idf[term] * numerator / denominator
     
     return scores
 
